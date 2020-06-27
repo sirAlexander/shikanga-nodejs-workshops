844d1003ae7ad712788060620d43d15721eb098a
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-06-27 16:26:21 +0100

    discovering node OS module

--- first-app/app.js
+++ first-app/app.js
@@ -1,7 +1,11 @@
 const log = require('./logger');
 const path = require('path');
+const os = require('os');
 
 var pathObj = path.parse(__filename);
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
 
 function sayHello(name) {
     log('Hello ' + name);
@@ -12,3 +16,12 @@ sayHello('Mosh');
 log(module);
 
 log(pathObj);
+
+log('Total Memory: ' + totalMemory);
+
+// or use
+// Template string
+// ES6 / ES2015 : ECMAScript 6
+
+log(`Total Memory: ${totalMemory}`);
+log(`Free Memory: ${freeMemory}`);
