0bdddbee3f5bbf87074008b1a1a0e89f2699b688
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-06-29 19:15:13 +0100

    Input validation with the awesome Joi library

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -31,12 +32,17 @@ app.get('/api/posts/:year/:month', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-    if(!req.body.name || req.body.name.length < 3){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+    if(result.error){
         // 400 Bad Request
-        res.status(400).send('Name is required and should be minimum 3 characters.');
+        res.status(400).send(result.error.details[0].message);
         return;
     }
-    
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
