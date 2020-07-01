e7290673774fdd1313c62c7ebc85d3d4e07ac403
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-07-01 20:08:09 +0100

    setting Environments and using different profiles

--- express-demo/index.js
+++ express-demo/index.js
@@ -6,12 +6,19 @@ const logger = require('./logger');
 
 const app = express();
 
+console.log(`app: ${app.get('env')}`);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(helmet());
-app.use(morgan('tiny'));
+
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...');
+}
+
 
 app.use(logger);
 
