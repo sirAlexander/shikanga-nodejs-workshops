a14056084427d11681c06d68edf434a861e16456
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-07-01 19:59:56 +0100

    how to install and use 3rd party middleware

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,11 +1,17 @@
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require('joi');
-const logger = require('./logger');
 const express = require('express');
+const logger = require('./logger');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use(helmet());
+app.use(morgan('tiny'));
 
 app.use(logger);
 
