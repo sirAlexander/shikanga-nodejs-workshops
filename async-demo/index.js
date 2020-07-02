06552835c71e18bcc1d9d1d8af005add062bb5f3
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-07-02 21:29:44 +0100

    replacing callbacks with promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -17,30 +17,35 @@ function displayUser(user){
 }
 
 
-
-
 // The 3 patterns used to deal with asynchronous code
 // 1. Callbacks
 // 2. Promises
 // 3. Async/await
 
-function getUser(id, callback){
-    setTimeout(() => {
-        console.log('Reading a user from a database...');
-        callback({ id: id, gitHubUsername: 'mosh' });
-    }, 2000);
+function getUser(id){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({ id: id, gitHubUsername: 'mosh' });
+        }, 2000);
+    });
 }
 
-function getRepositories(username, callback){
-    setTimeout(() => {
-        console.log('Calling GitHub API...');
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 2000);
+function getRepositories(username){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
 }
 
-function getCommits(repo, callback){
-    setTimeout(() => {
-        console.log('Getting repository commits...');
-        callback(['commit1', 'commit2', 'commit3']);
-    }, 2000);
+function getCommits(repo){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Getting repository commits...');
+            resolve(['commit1', 'commit2', 'commit3']);
+        }, 2000);
+    });
+
 }
