ecbd5f2b5a2508c93aa58daf67abb36bf5893d29
sir'Alexander <alexander@shikanga.co.uk>
2021-10-30 13:19:43 +0000

    explored a alternate approach to exporting modules

--- coding-addict/node-express-tutorial/tutorial/app.js
+++ coding-addict/node-express-tutorial/tutorial/app.js
@@ -2,6 +2,9 @@
 // Modules - Encapsulated Code (only share minimum)
 const names = require('./4-names');
 const sayHi = require('./5-utils');
+const data = require('./6-alternative-approach');
+
+console.log(data);
 
 
 sayHi('susan');
