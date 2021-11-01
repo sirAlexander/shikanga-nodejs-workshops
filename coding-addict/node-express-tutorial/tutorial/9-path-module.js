9dc75e2f1371fc693b44d4afea20172e082f26bc
sir'Alexander <alexander@shikanga.co.uk>
2021-11-01 19:34:49 +0000

    covered path module

---
+++ coding-addict/node-express-tutorial/tutorial/9-path-module.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+// path separator
+console.log(path.sep);
+
+// path join
+const filePath = path.join('/content', 'subfolder', 'test.txt');
+console.log(filePath);
+
+const base = path.basename(filePath);
+console.log(base);
+
+// path resolve
+const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt');
+console.log(absolute);
+console.log(__dirname);
